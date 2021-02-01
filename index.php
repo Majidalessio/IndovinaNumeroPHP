6bc3c586b8703f1114f87faff51567b519b016d5
Majidalessio <50731803+Majidalessio@users.noreply.github.com>
2021-02-01 13:01:22 +0100

    Final Version

--- index.php
+++ index.php
@@ -1,28 +1,48 @@
 <?php
 
 function graphics()
-{
-    $maxntries=2;      //number of tries
-    $maxnum=10;     //max number;
-    $ntriesLabel="tentativo numero: ";      //variable used in order to speed up the title changes
+{ 
+    $min=1;
+    $max=10;
+    $nIn='';
+    $ntries='';
+    $score="";
+    $maxntries=4;                       //number of tries
+    $ntriesLabel="tentativo numero: ";  //variable used in order to speed up the title changes
     if(isset($_POST['submit']))         //it means that on click of the submit button it does the following code
-    {     
+    {
+       $rand=$_POST['randNum'];  
        $ntries=$_POST['ntent'];
+       $nIn=$_POST['numIn'];
        if($ntries==0)
        {
            $ntries=1;
        }
        else if($ntries<=$maxntries && $ntries>0)
        {
-           
+          $ntries+=1;
+          if($nIn<$rand)
+            {
+             $score="Il numero da indovinare è maggiore!";          
+            }
+          if($nIn>$rand)
+            {
+              $score="Il numero da indovinare è minore!";
+            }
+            if($nIn==$rand)
+            {
+              $score="Congratulazioni hai vinto!!";
+            }
        }
        else
        {
-           
+           $score="Hai perso, ritenta!";
        }
     }
-    else            //this code will be generated the first time you load the page
+    else                                    //this code will be generated the first time you load the page
     {
+        $rand= rand($min, $max);            //generate the rand number when you first open the page
+        $nIn='';
         $ntries=1;
     }
     
@@ -40,13 +60,16 @@ $page= <<<HTML_FORM
             <div id="title">Gioco dell'indovina numero</div>
             <div  id="form"></br></br>
                 <b>Regole del gioco:</b> Si deve indovinare nel minor numero 
-                di tentativi un numero compreso fra 0 e $maxnum estratto casualmente dal sistema</br>
+                di tentativi un numero compreso fra $min e $max estratto casualmente dal sistema</br>
                 <form novalidate method="POST" action="{$_SERVER['PHP_SELF']}">                     <!--method used post, action means that the page will reload itself PHP_SELF-->
                     <fieldset class="field">
                         <label><b>$ntriesLabel $ntries</b></label>
                         <input type="hidden" name="ntent" value='$ntries'></input>                  <!-- hidden input used in order to show the number of tries-->
-                        <input type="number" id="inputNumber" name="numIn" required></input>        <!-- this is the input used to read the number from the client, the input must be a number (tag required forces it) -->
+                        <input type="hidden" name="randNum" value='$rand'></input>
+                        <input type="number" id="inputNumber" name="numIn" required placeholder="inserisci il numero" value="$nIn"></input>        <!-- this is the input used to read the number from the client, the input must be a number (tag required forces it) -->
                         <button id="checkbutton" type="submit" name="submit">Check It!</button>
+                        <button type="reset" onclick="window.location.reload()">Reset</button>
+                        <div><label><b>$score</b></label></div>
                     </fieldset>
                 </form>
             </div>
